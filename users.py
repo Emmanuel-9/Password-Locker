2f6644883613611fe512fb4389e3091839e66cc4
Emmanuel-9 <oregadaniel181@gmail.com>
2020-06-29 09:42:27 +0300

    Create class User and its init function.

---
+++ users.py
@@ -0,0 +1,16 @@
+class User:
+  '''
+  Class that generates new instances of users
+  '''
+
+  usersList = []
+
+  def __init__(self,username,password):
+    '''
+    Defining attributes of the class
+    '''
+    self.username = username 
+    self.password = password
+
+
+  
